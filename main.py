c1e3b429fbe899d96bcf2d751a5b8257e4ae25cd
shbunyod2197 <shbunyod2197@gmail.com>
2025-12-05 15:08:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1-m
+for i in range(5):
+    print("$ " * 5)
+# 2-m
+for i in range(1, 6):
+    print(f"{i}" * i)
+# 3-m
+for i in range(2, 5):
+    print(f"@" * i, end="\n")
+
+for i in range(2, 1, -1):
+    print(f"@" * i)
+# 4-m
+for i in range(1, 5):
+    print(f"{i }" * i,"#" )
